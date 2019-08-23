af9a30eedcf037d25f4752489df5421d16d3904d
Siqi Zhang <iqis.gnahz@gmail.com>
2019-08-23 12:08:14 -0400

    some charaltan fucntions

---
+++ R/charlatan.R
@@ -0,0 +1,151 @@
+#' Functions Exported from charlatan
+#'
+#' @name charlatan
+#'
+NULL
+
+
+# person name
+#' @export
+#' @rdname charlatan
+name_person <- charlatan::ch_name
+
+# company name
+#' @export
+#' @rdname charlatan
+name_company <- charlatan::ch_company
+
+# job title
+#' @export
+#' @rdname charlatan
+job <- charlatan::ch_job
+
+# currency
+#' @export
+#' @rdname charlatan
+currency <- charlatan::ch_currency
+
+# phone number
+#' @export
+#' @rdname charlatan
+phone_number <- charlatan::ch_phone_number
+
+# credit card
+#' @export
+#' @rdname charlatan
+cc_provider <- charlatan::ch_credit_card_provider
+
+#' @export
+#' @rdname charlatan
+cc_number <- charlatan::ch_credit_card_number
+
+#' @export
+#' @rdname charlatan
+cc_security_code <- charlatan::ch_credit_card_security_code
+
+
+
+# colors
+#' @export
+#' @rdname charlatan
+color_name <- charlatan::ch_color_name
+
+#' @export
+#' @rdname charlatan
+color_name_safe<- charlatan::ch_color_name
+
+#' @export
+#' @rdname charlatan
+color_hex <- charlatan::ch_hex_color
+
+#' @export
+#' @rdname charlatan
+color_safe_hex <- charlatan::ch_safe_hex_color
+
+#' @export
+#' @rdname charlatan
+color_rgb <- charlatan::ch_rgb_color
+
+#' @export
+#' @rdname charlatan
+color_rgb_css <- charlatan::ch_rgb_css_color
+
+
+
+# coordinates
+#' @export
+#' @rdname charlatan
+lat <- charlatan::ch_lat
+
+#' @export
+#' @rdname charlatan
+lon <- charlatan::ch_lon
+
+#' @export
+#' @rdname charlatan
+position <- charlatan::ch_position
+
+# date times
+#' @export
+#' @rdname charlatan
+timezone <- charlatan::ch_timezone
+
+#' @export
+#' @rdname charlatan
+unix_time <- charlatan::ch_unix_time
+
+#' @export
+#' @rdname charlatan
+date_time <- charlatan::ch_date_time
+
+
+# doi
+#' @export
+#' @rdname charlatan
+doi <- charlatan::ch_doi
+
+
+# taxonomy
+#' @export
+#' @rdname charlatan
+taxo_genus <- charlatan::ch_taxonomic_genus
+
+#' @export
+#' @rdname charlatan
+taxo_epithet <- charlatan::ch_taxonomic_epithet
+
+#' @export
+#' @rdname charlatan
+taxo_species <- charlatan::ch_taxonomic_species
+
+# gene sequences
+#' @export
+#' @rdname charlatan
+gene_sequence <- charlatan::ch_gene_sequence
+
+
+
+# number
+#' @export
+#' @rdname charlatan
+double <- charlatan::ch_double
+
+#' @export
+#' @rdname charlatan
+integer <- charlatan::ch_integer
+
+#' @export
+#' @rdname charlatan
+uniform <- charlatan::ch_unif
+
+#' @export
+#' @rdname charlatan
+norm <- charlatan::ch_norm
+
+#' @export
+#' @rdname charlatan
+lnorm <- charlatan::ch_lnorm
+
+#' @export
+#' @rdname charlatan
+beta <- charlatan::ch_double
